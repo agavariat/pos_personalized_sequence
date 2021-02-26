2892787f62141a754ab5a7d32529816f1b3df1bc
NelsonV <nelson.avelez@hotmail.com>
2021-02-26 16:44:20 -0500

    Resolucion POS

--- models/pos.py
+++ models/pos.py
@@ -7,6 +7,7 @@ class PosConfig(models.Model):
 
     personalized_sequence_id = fields.Many2one('ir.sequence', 'Personalized Order IDs Sequence')
     refund_sequence_id = fields.Many2one('ir.sequence', 'Refund personalized Order IDs Sequence')
+    resolucion_id = fields.Char('Resolucion POS')
 
 
 class PosOrder(models.Model):
@@ -15,3 +16,5 @@ class PosOrder(models.Model):
     def pos_personalized_sequence(self, sequence):
         sequence = self.env['ir.sequence'].sudo().browse([sequence])
         return sequence.next_by_id()
+
+
